974ef4c7cd7e5d7f862e66481d181545be66924e
rcb269 <marvelstudiom64@gmail.com>
2026-01-16 20:24:41 +0530

    uicksort

--- DSA/recursion.cpp
+++ DSA/recursion.cpp
@@ -260,7 +260,7 @@ int main(){
     }
 }*/
 //merge sort
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 void merge(int *arr,int s,int e){
     int mid=s+(e-s)/2;
@@ -317,7 +317,58 @@ void merge(int *arr,int s,int e){
             for(int i=0;i<n;i++){
                 cout<<arr[i]<<" ";
             }cout<<endl;
+        }*/
+//quicksort
+#include<iostream>
+using namespace std;
+void print(int arr[],int s,int e){
+    for(int i=s;i<=e;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+}
+int partition(int arr[],int s,int e){
+    int pivot=arr[s];
+    int cnt=0;
+    for(int i=s+1;i<=e;i++){
+        if(arr[i]<=pivot){
+            cnt++;
         }
+    }
+    //place pivot at right position
+    int pivotindex=s+cnt;
+    swap(arr[pivotindex],arr[s]);
+    //left and right part
+    int i=s,j=e;
+    while(i<pivotindex && j>pivotindex){
+        while(arr[i]<=pivot){ i++;}
+        while(arr[j]>pivot){j--;}
+        if(i<pivotindex && j>pivotindex){
+            swap(arr[i++],arr[j--]);
+        }
+    }
+    return pivotindex;
+}
+
+void quicksort(int arr[],int s,int e ){
+    print(arr,s,e);
+   if(s>=e){ return ;}
+
+    int p=partition(arr,s,e);
+    quicksort(arr,s,p-1);
+    quicksort(arr,p+1,e);
+   }
+int main(){
+    int arr[]={6,5,4,3,2,1};
+    int n=6;
+    quicksort(arr,0,n-1);
+    cout<<"sorted array"<<endl;
+    for(int i =0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+}
+
+
+
 
 
 

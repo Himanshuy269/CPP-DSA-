cfb09cb9246f8e8b48ff56c7964042bf9d1d4140
rcb269 <marvelstudiom64@gmail.com>
2026-02-10 23:40:47 +0530

    insertionathead

---
+++ DSA/linkedList.cpp
@@ -0,0 +1,33 @@
+//insertion at head
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+    node(int data){
+        this -> data= data;
+        this -> next= NULL;
+    }
+};
+void insertathead(node* &head,int data){
+      node* temp= new node(data);
+      temp-> next=head;
+      head=temp;
+}
+void print( node* &head){
+    node* temp= head;
+    while(temp!=NULL){
+        cout<<temp -> data<<endl;
+        temp=temp ->  next;
+    }
+}
+int main(){
+node* node1=new node(10); //create a new node
+//cout<<node1 -> data << endl;
+//cout<<node1 -> next <<endl;
+node* head=node1;
+print(head);
+insertathead(head,12);
+print(head);
+}

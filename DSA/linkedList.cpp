3c8697f408f00d1c42d85a59e5cd6e51b8f8140b
rcb269 <marvelstudiom64@gmail.com>
2026-02-15 20:25:15 +0530

    insertingattail(doublyLinkedList)

--- DSA/linkedList.cpp
+++ DSA/linkedList.cpp
@@ -287,3 +287,65 @@ insertathead(head,8);
 print(head);
 return 0;
 }*/
+ //insertattail
+/*#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* prev;
+    node* next;
+    //constructor
+    node(int data){
+        this->data=data;
+        this->prev=NULL;
+        this->next=NULL;
+    }
+};
+
+void print(node* head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"   ";
+        temp=temp->next;
+    }
+cout<<endl;
+}
+//length of linked list
+int getlength(node* head){
+    int len=0;
+    node* temp=head;
+    while(temp!=NULL){
+        len++;
+        temp=temp->next;
+    }
+    return len;
+}
+void insertathead(node* &head,int data){
+    node* temp=new node(data);
+    temp->next=head;
+    head->prev=temp;
+    head=temp; 
+}
+void insertattail(node* &tail,int data){ //insertingattail
+    node* temp=new node(data);
+    tail->next=temp;
+    temp->prev=tail;
+    tail=temp;
+}
+int main(){
+node* node1=new node(10);
+node* head=node1;
+node* tail=node1;
+print(head);
+cout<<getlength(head)<<endl;
+insertathead(head,11);
+print(head);
+insertathead(head,13);
+print(head);
+insertathead(head,8);
+print(head);
+insertattail(tail,14);
+print(head);
+return 0;
+}*/

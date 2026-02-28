6137e1a210426c8436fa2a64dc87598224a532f2
rcb269 <marvelstudiom64@gmail.com>
2026-02-28 17:54:25 +0530

    inserting node in circular linked List

--- DSA/linkedList.cpp
+++ DSA/linkedList.cpp
@@ -541,7 +541,7 @@ print(head);
 return 0;
 }*/
 //deleting node in doubly linked list
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 class node{
     public:
@@ -582,9 +582,6 @@ void deletenode(int position,node* &head,node* &tail){
             curr=curr-> next;
             cnt++;
         }
-        if(curr->next==NULL){ //If deleting last node
-            tail=prev;
-            prev->next=NULL;
         }
         else{
             //middle node
@@ -683,6 +680,96 @@ print(head);
 deletenode(3,head,tail);
 print(head);
 return 0;
+}*/
+//circular Linked List
+//inserting node
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;        
+    node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+    ~node(){
+        int value=this->data;
+        if(this->next!=NULL){
+            delete next;
+            next=NULL;
+        }
+        cout<<"memory is free for node"<<value<<endl;
+    }
+
+};
+void insertnode(node* &tail,int element,int data){
+    //assuming element is present 
+    //if list is empty 
+    if(tail==NULL){
+        node* newnode=new node(data);
+        tail=newnode;
+        newnode->next=newnode;
+    }
+    else{
+        //non-empty List
+        //assuming that the element is present in the list
+        node* curr=tail;
+        while(curr->data!=element){
+            curr=curr->next;
+        }
+        //element found -> curr is representing elemnt wla node
+        node* temp=new node(data);
+        temp->next=curr->next;
+        curr->next=temp;
+    }    
 }
+void print(node* tail){
+    node* temp=tail;
+    do{
+        cout<<tail->data<<" ";
+        tail=tail->next;  
+    }
+     while(tail!=temp);
+    //{
+    //     cout<<tail->data<<" ";
+    //     tail=tail->next;
+    // }
+    cout<<endl;
+}
+int main(){
+node* tail=NULL;
+insertnode(tail,5,3);
+print(tail);
+insertnode(tail,3,5);
+print(tail);
+insertnode(tail,5,7);
+print(tail);
+insertnode(tail,7,9);
+print(tail);
+insertnode(tail,5,6);
+print(tail);
+insertnode(tail,9,10);
+print(tail);
+insertnode(tail,3,4);
+print(tail);
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

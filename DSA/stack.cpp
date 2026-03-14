ebbe3d63e627bd66c5035c34d1a5e435eeb91939
rcb269 <marvelstudiom64@gmail.com>
2026-03-14 19:50:26 +0530

    implementation of stack

--- DSA/stack.cpp
+++ DSA/stack.cpp
@@ -16,3 +16,71 @@ int main(){
     cout<<"size of stack is "<<s.size()<<endl;
 
 }*/
+//implementation of stack using array
+#include<iostream>
+#include<stack>
+using namespace std;
+class Stack{
+    public:
+int *arr;
+int top;
+int size;
+//behaviour
+Stack(int size){
+    this->size=size;
+    arr=new int[size];
+    top=-1;
+}
+void push(int element){
+    if(size-top>1){
+        top++;
+        arr[top]=element;
+    }
+    else{
+        cout<<"stack overflow";
+    }
+}
+void pop(){
+    if(top>=0){
+        top--;
+    }
+    else{
+        cout<<"stack underflow";
+    }
+}
+int peek(){
+    if(top>=0 && top<size){
+        return arr[top];
+    }
+    else{
+        cout<<"stack is empty"<<endl;
+        return -1;
+    }
+}
+bool isempty(){
+    if(top == -1){
+        return true;
+    }
+    else return false;
+}
+};
+int main(){
+    Stack st(5);
+    st.push(22);
+    st.push(43);
+    st.push(44);
+    cout<<st.peek()<<endl;
+    st.pop();
+    cout<<st.peek()<<endl;
+    st.pop();
+    cout<<st.peek()<<endl;
+    st.pop();
+    cout<<st.peek()<<endl;
+    if(st.isempty()){
+        cout<<"stack is empty";
+    }
+    else{
+        cout<<"stack is not empty";
+    }
+return 0;
+}

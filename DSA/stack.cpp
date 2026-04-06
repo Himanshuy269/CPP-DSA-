754055bf2793d15f439e9ca55ba66d7a2cd34dd6
rcb269 <marvelstudiom64@gmail.com>
2026-04-06 20:49:03 +0530

    reverse a string using stack

--- DSA/stack.cpp
+++ DSA/stack.cpp
@@ -17,7 +17,7 @@ int main(){
 
 }*/
 //implementation of stack using array
-#include<iostream>
+/*#include<iostream>
 #include<stack>
 using namespace std;
 class Stack{
@@ -83,4 +83,23 @@ int main(){
         cout<<"stack is not empty";
     }
 return 0;
+}*/
+//#reverse a string using stack
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    string str="love";
+    stack<char> s;
+    for(int i=0;i<str.length();i++){
+     char ch=str[i];
+     s.push(ch);
+    }
+    string ans="";
+    while(!s.empty()){
+        char ch=s.top();
+        ans.push_back(ch);
+        s.pop();
+    }
+    cout<<"answer is: "<<ans<<endl;
 }

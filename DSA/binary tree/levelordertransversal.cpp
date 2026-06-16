1f0a5a8016ab039b4e15699cb8db6e9e23bd71e2
rcb269 <marvelstudiom64@gmail.com>
2026-06-16 08:28:59 +0530

    levelOrderTransverse

---
+++ DSA/binary tree/levelordertransversal.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+    node(int d){
+        this->data=d;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+node* buildTree(node* root){
+    cout<<"enter the data"<<endl;
+    int data;
+    cin>>data;
+    root=new node(data);
+    if(data==-1){
+        return NULL;
+    }
+    cout<<"enter the data for inserting in left node "<<data<<endl;
+    root->left=buildTree(root->left);
+    cout<<"Enter the data for inserting in right node "<<data<<endl;
+    root->right=buildTree(root->right);
+    return root; 
+}
+void levelOrderTransversal(node* root){
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        node* temp=q.front();
+        q.pop();
+        if(temp==NULL){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+            else{
+                cout<<temp->data<<" ";
+                if(temp->left){
+                    q.push(temp->left);
+                }
+                if(temp->right){
+                    q.push(temp->right);
+                }
+            }
+        }
+    }
+int main(){
+  node* root=NULL;
+  root=buildTree(root);
+  cout<<"level order transversal of binary tree"<<endl;
+  levelOrderTransversal(root);
+  return 0;
+}
